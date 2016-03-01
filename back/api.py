301db5c95daa75452be81db7973dc928b6d80c7f
Cris Stringfellow <dosaygo@2TRILLIONHUMANS.local>
2016-03-01 13:27:00 +0800

    Specified more end point paths, linked form action to create end point.

--- back/api.py
+++ back/api.py
@@ -6,10 +6,26 @@ from webapp2 import (
   )
 
 class collection_endpoint( endpoint ):
-  pass
+  def get( self, type, cursor = None ):
+    """Get the first or next page of the collection"""
+    self.response.write( 'getting collection<br>' )
+    self.response.write( type )
+    self.response.write( '<br>' )
+    self.response.write( cursor )
 
 class instance_endpoint( endpoint ):
-  pass
+  def get( self, type, id ):
+    """ Get a specific instance """
+    pass
+
+  def post( self, type, id = 'new' ):
+    """ Create or update a specific instance """
+    if id == 'new':
+      self.response.write( 'creating<br>' )
+      self.response.write( self.request.POST.mixed() )
+    else:
+      self.response.write( 'updating<br>' )
+      self.response.write( self.request.POST.mixed() )
 
 class role_endpoint( endpoint ):
   pass
@@ -26,8 +42,19 @@ class test_endpoint( endpoint ):
 # An optimization is to group these paths under their common prefix
 paths = [
   path( '/api/model/type/<type>/role/<role>', handler=role_endpoint ),
+  path( '/api/model/type/<type>',
+      handler=instance_endpoint, 
+      methods=['POST'] 
+    ),
+  path( '/api/model/type/<type>', 
+      handler=collection_endpoint, 
+      methods=['GET'] 
+    ),
+  path( '/api/model/type/<type>/cursor/<cursor>', 
+      handler=collection_endpoint, 
+      methods=['GET'] 
+    ),
   path( '/api/model/type/<type>/id/<id>', handler=instance_endpoint ),
-  path( '/api/model/type/<type>', handler=collection_endpoint ),
   path( '/api/test', handler=test_endpoint )
 ]
 
