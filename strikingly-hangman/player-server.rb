2985d24434adf368af6903a1ebd20f9c30e37bb4
Cris Stringfellow <dosaygo@2TRILLIONHUMANS.local>
2016-03-02 19:48:40 +0800

    Added a servlet to the server.

--- strikingly-hangman/player-server.rb
+++ strikingly-hangman/player-server.rb
@@ -1,12 +1,19 @@
 require 'webrick'
 
-root = File.expand_path './'
+class Player < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET request, response
+    response.status = 200
+    response[ 'Content-Type' ] = 'text/plain'
+    response.body = 'Hello, Awesome!'
+  end
+end
 
-server = WEBrick::HTTPServer.new :Port => 8080, :DocumentRoot => root
+server = WEBrick::HTTPServer.new :Port => 8080
 
 trap 'INT' do server.shutdown end
 
-server.start
+server.mount '/play', Player
 
+server.start
 
 
