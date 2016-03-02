4c11a93faba1caab6c0ea86d2c39d77906e38145
Populace SoHo <popsoho@dosaygo-mac-0-dev.populace-soho.com>
2016-03-02 16:16:44 +0800

    Added empty initial ruby server.

---
+++ strikingly-hangman/player-server.rb
@@ -0,0 +1,12 @@
+require 'webrick'
+
+root = File.expand_path './'
+
+server = WEBrick::HTTPServer.new :Port => 8080, :DocumentRoot => root
+
+trap 'INT' do server.shutdown end
+
+server.start
+
+
+
