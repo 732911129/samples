cd97b77c63361f35cfb5e194c3e69993b4513f88
Cris Stringfellow <dosaygo@2TRILLIONHUMANS.local>
2016-03-05 14:16:39 +0800

    I was going this wrong. Maxing at 10 total guesses, instead of 10 wrong guesses. Ahahaha.

--- strikingly-hangman/index.py
+++ strikingly-hangman/index.py
@@ -150,7 +150,7 @@ def count_keys_per_key( keys ):
     count[ 'value' ] += 1
   return counts
 
-def calculate_letter_entropies( keys ):
+def calculate_letter_entropies( keys, words ):
   """ For each key, its probability is
   key_count / total_keys
   And its entropy follows
@@ -161,6 +161,9 @@ def calculate_letter_entropies( keys ):
   letter_scores = dict()
   letter_entropies = dict()
   letter_key_counts = dict()
+  total_keys = 0
+  total_words = float( len( words ) )
+  total_entropy = 0
   for key in key_counts:
     count = key_counts[ key ][ 'value' ]
     letter = key[ 0 ]
@@ -176,44 +179,61 @@ def calculate_letter_entropies( keys ):
     except:
       key_counter = letter_score[ 'keys' ][ key ] = { 'value' : count }
     letter_score[ 'total' ] += count
+    total_keys += count
   for letter in letter_scores:
     letter_score = letter_scores[ letter ]
     letter_keys = letter_score[ 'keys' ]
     letter_entropy = letter_entropies[ letter ] = { 'value' : 0 }
     total = float( letter_score[ 'total' ] )
-    letter_key_counts[ letter ] = { 'value' : total }
+    p_letter = total / total_keys 
+    letter_key_counts[ letter ] = { 'value' : p_letter }
     for key in letter_keys:
       key_count = letter_keys[ key ]
-      p_key = key_count[ 'p' ] = key_count[ 'value' ] / total
+      p_key = key_count[ 'p' ] = key_count[ 'value' ] / total_words
       letter_entropy[ 'value' ] -= p_key * math.log( p_key )
+    total_entropy += letter_entropy[ 'value' ]
+  S = max( 1, total_entropy )
+  for letter in letter_entropies:
+    letter_entropy = letter_entropies[ letter ]
+    E = letter_entropy[ 'value' ]
+    p = letter_key_counts[ letter ][ 'value' ]
+    # convert to ( pE ) + ( 1 - p ) ( S - E )
+    letter_entropy[ 'value' ]  = E/S
   print letter_scores
   return letter_entropies, letter_key_counts
 
-def merge_and_score( keys ):
+def merge_and_score( keys, words ):
   # return a set
   # sort then count 
   keys = sorted( keys, key=lambda c : ( c[ 0 ], c[ 1 ] ) )
-  letter_entropies, letter_key_counts = calculate_letter_entropies( keys )
+  letter_entropies, letter_key_counts = calculate_letter_entropies( keys, words )
   return ( letter_entropies, letter_key_counts )
 
 def guess( mask, already_tried, display_only = False ):
   candidate_words, fallback = query( mask, table, len( mask ), already_tried )
-  letter_entropies, letter_key_counts = merge_and_score( [ key for word in candidate_words for key in produce_keys( word ) ] ) 
+  letter_entropies, letter_key_counts = merge_and_score( [ key for word in candidate_words for key in produce_keys( word ) ], candidate_words ) 
   sorted_counts = sort_counts( remove_entries( letter_key_counts, already_tried ) )
   sorted_entropies = sort_counts( remove_entries( letter_entropies, already_tried ) )
-  sorted_fallback_counts = []
+  sorted_fallback_entropies = []
   if not sorted_counts:
     try:
-      unique_fallback_keys = merge_and_score( [ key for word in fallback for key in produce_keys( word ) ] )
+      fallback_entropies, fallback_key_counts = merge_and_score( [ key for word in fallback for key in produce_keys( word ) ], fallback )
     except:
       unique_fallback_keys = set()
-    fallback_letter_counts = remove_entries( count_keys_per_letter( unique_fallback_keys ), already_tried )
-    sorted_fallback_counts = sort_counts( fallback_letter_counts )
+    sorted_fallback_entropies = sort_counts( remove_entries( fallback_entropies, already_tried ) )
   if not display_only:
+    guesses = sorted( sorted_entropies, key = lambda c : ( 
+        c[ 1 ][ 'value' ] )
+      )
+    fallback_guesses = sorted_fallback_entropies
+    if not guesses:
+      fallback_guesses = sorted( sorted_fallback_entropies, key = lambda c : ( 
+          c[ 1 ][ 'value' ] ) 
+        )
     val = { 
-      'guesses': sorted_entropies, 
+      'guesses': guesses, 
       'key_counts' : sorted_counts,
-      'fallback': sorted_fallback_counts 
+      'fallback': fallback_guesses
     }
     print val
     return val
