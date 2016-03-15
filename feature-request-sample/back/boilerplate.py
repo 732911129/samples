b28f3261d40a3161c2605e4d88a1c2274f9695d6
Cris Stringfellow <humanarity@gmail.com>
2016-03-15 22:27:15 +0800

    Completed printer initial unrefined.

--- feature-request-sample/back/boilerplate.py
+++ feature-request-sample/back/boilerplate.py
@@ -1,21 +1,21 @@
 head_after_begin_meta_charset_boilerplate = """
-    <meta charset=utf-8>
+    <meta charset=utf-8 />
   """
 
 head_after_begin_meta_viewport_boilerplate = """
-    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name=viewport content="width=device-width, initial-scale=1" />
   """
 
-head_before_end_styles_boilerplate = """
-    <link rel=stylesheet href=/component-test-colors.css>
-    <link rel=stylesheet href=/component-basic-compatible.css>
-    <link rel=stylesheet href=/component-modern-upgrade.css>
-    <link rel=stylesheet href=/component-basic-layout.css>
-  """
+head_before_end_styles_boilerplate = [
+    """<link rel=stylesheet href=/component-test-colors.css />""",
+    """<link rel=stylesheet href=/component-basic-compatible.css />""",
+    """<link rel=stylesheet href=/component-modern-upgrade.css />""",
+    """<link rel=stylesheet href=/component-basic-layout.css />"""
+  ]
 
 body_after_end_scripts_boilerplate = """
-    <script src=/resize_embedder.js></script>
-  """
+  <script src=/resize_embedder.js></script>
+"""
 
 boilerplate = {
   'head' : {
