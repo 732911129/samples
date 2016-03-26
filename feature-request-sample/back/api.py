847c8e1099ba72347ebc1d2f7588ec26ce6ab049
Cris Stringfellow <humanarity@gmail.com>
2016-03-26 21:25:27 +0800

    Factored out the prefix from the media paths.

--- feature-request-sample/back/api.py
+++ feature-request-sample/back/api.py
@@ -1,19 +1,25 @@
 import os
+
 from webapp2 import (
     WSGIApplication as server,
     Route as path,
     RequestHandler as endpoint
   )
+
+from webapp2_extras.routes import (
+    PathPrefixRoute as prefix 
+  )
+
 from models import Media
 
 EXTENSIONS = {
-  'css' : 'text/css',
-  'html' : 'text/html',
-  'txt' : 'text/plain',
-  'jpg' : 'image/jpg',
-  'png' : 'image/png',
-  'ico' : 'image/x-icon'
-}
+    '.css' : 'text/css',
+    '.html' : 'text/html',
+    '.txt' : 'text/plain',
+    '.jpg' : 'image/jpg',
+    '.png' : 'image/png',
+    '.ico' : 'image/x-icon'
+  }
 
 class collection_endpoint( endpoint ):
   def get( self, media_type, cursor = None ):
@@ -30,52 +36,55 @@ class instance_endpoint( endpoint ):
     self.response.write( Media.render( media_type = media_type, id = id, params = self.request.POST.mixed() ) )
 
 class catchall_endpoint( endpoint ):
-  def get( self, path = None ):
+  def render_path( self, path = None ):
     name, ext = os.path.splitext( path )
-    mime = 'text/html'
-    ext = ext or ''
-    ext = ext[1:]
-    if ext in EXTENSIONS:
+    try:  
       mime = EXTENSIONS[ ext ]
+    except KeyError:
+      mime = 'text/html'
     self.response.headers[ 'Content-Type' ] = mime
     self.response.write( Media.render( path = path ) )
+
+  def get( self, path = None ):
+    self.render_path( path )
 
   def post( self, path = None ):
-    name, ext = os.path.splitext( path )
-    mime = 'text/html'
-    ext = ext or ''
-    ext = ext[1:]
-    if ext in EXTENSIONS:
-      mime = EXTENSIONS[ ext ]
-    self.response.headers[ 'Content-Type' ] = mime
-    self.response.write( Media.render( path = path ) )
+    self.render_path( path )
 
-# An optimization is to group these paths under their common prefix
+media_paths = prefix( '/api/media',
+    [
+        path( '/type/<media_type><:/?>', 
+            methods=['GET'],
+            handler=collection_endpoint
+          ),
+        path( '/type/<media_type>/cursor/<cursor><:/?>', 
+            methods=['GET'],
+            handler=collection_endpoint
+          ),
+        path( '/type/<media_type><:/?>',
+            methods=['POST'],
+            handler=instance_endpoint
+          ),
+        path( '/type/<media_type>/id/<id><:/?>', 
+            methods=['GET','POST'],
+            handler=instance_endpoint
+          ),
+      ]
+  )
 
-paths = [
-  path( '/api/media/type/<media_type><:/?>', 
-      methods=['GET'],
-      handler=collection_endpoint
-    ),
-  path( '/api/media/type/<media_type>/cursor/<cursor><:/?>', 
-      methods=['GET'],
-      handler=collection_endpoint
-    ),
-  path( '/api/media/type/<media_type><:/?>',
-      methods=['POST'],
-      handler=instance_endpoint
-    ),
-  path( '/api/media/type/<media_type>/id/<id><:/?>', 
-      methods=['GET','POST'],
-      handler=instance_endpoint
-    ),
-  path( '/<path:.*>',
-      methods=['GET','POST'],
-      handler=catchall_endpoint
-    )
-]
+catch_all = path( 
+    '/<path:.*>',
+    methods=['GET','POST'],
+    handler=catchall_endpoint
+  )
 
-app = server( paths, debug = True )
+app = server( 
+    [
+        media_paths,
+        catch_all
+      ]
+    , debug = True 
+  )
 
 if __name__ == "__main__":
   app.run()
