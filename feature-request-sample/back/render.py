34e98dd78384704c1d3659818275ed0ed2d75fa7
Cris Stringfellow <humanarity@gmail.com>
2016-03-31 16:53:13 +0800

    Sketched rough outline of render flow using new transformer based path.

--- feature-request-sample/back/render.py
+++ feature-request-sample/back/render.py
@@ -1,5 +1,25 @@
 from parser import ImprintingParser
 
-def imprint( model, view ):
-  imprinter = ImprintingParser()
-  return imprinter.imprint( model, view )
+from boilerplating_transformer import BoilerplatingTransformer
+from indexing_transformer import IndexingTransformer
+from projecting_transformer import ProjectingTransformer
+
+b = BoilerPlatingTransformer()
+i = IndexingTransformer()
+p = ProjectingTransformer()
+
+def imprint( media, control ):
+  input = {
+      'doc' : control
+    }
+  boilerplated = b.transform( input )
+  indexed = i.transform( boilerplated )
+  indexed_mediated = {
+      'doc' : indexed[ 'doc' ],
+      'index' : indexed[ 'index' ],
+      'media' : media
+    }
+  projected = p.transform( indexed_mediated )
+  return projected[ 'doc' ] 
+  
+  
