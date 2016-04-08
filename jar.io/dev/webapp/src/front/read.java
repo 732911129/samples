c4a5750da841c8a29f5b0b2497f91c79b2ab675d
Cris Stringfellow <humanarity@gmail.com>
2016-04-08 20:56:15 +0800

    Now can read files.

---
+++ jar.io/dev/webapp/src/front/read.java
@@ -0,0 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class read {
+  public static void main( String args[] ) throws Exception {
+    Stream<String> lines = Files.lines( Paths.get( args[ 0 ] ) );
+    lines.forEach( s -> System.out.println( s ) );
+  }
+}
