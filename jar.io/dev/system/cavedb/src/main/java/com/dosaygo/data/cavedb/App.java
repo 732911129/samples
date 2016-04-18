057d4b95d727bd65943620071e707a547b7f0b43
Cris Stringfellow <humanarity@gmail.com>
2016-04-18 14:43:28 +0800

    First rough sketched utils of cave.

--- jar.io/dev/system/cavedb/src/main/java/com/dosaygo/data/cavedb/App.java
+++ jar.io/dev/system/cavedb/src/main/java/com/dosaygo/data/cavedb/App.java
@@ -1,13 +1,42 @@
 package com.dosaygo.data.cavedb;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+import java.io.IOException;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+
 /**
- * Hello world!
+ * CaveDb
  *
  */
+
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "Starting cave..." );
+        System.out.println( "ARG 0: " + args[ 0 ] );
+        System.out.println( Util.fileToString( args[ 0 ] ) );
+    }
+
+    public static class Util
     {
-        System.out.println( "Hello World!" );
+
+      public static String guid() {
+        return UUID.randomUUID().toString();
+      }
+
+      public static String fileToString( String... path ) throws IOException {
+        return Files
+          .lines( Paths.get( "", path ) )
+          .collect( Collectors.joining( "\n" ) );
+      }
+
     }
 }
